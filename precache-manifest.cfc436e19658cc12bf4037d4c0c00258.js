37ae3274371ec08d8f406019006d2103e452c7bd
Dmitriy Shved <dshved@hotmail.com>
2019-04-09 16:42:18 +0300

    Updates

---
+++ precache-manifest.cfc436e19658cc12bf4037d4c0c00258.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a21e0c0407692fd19735",
+    "url": "/static/js/main.2dd5bd67.chunk.js"
+  },
+  {
+    "revision": "634a699f814d729cd5f1",
+    "url": "/static/js/2.baa2ceff.chunk.js"
+  },
+  {
+    "revision": "a21e0c0407692fd19735",
+    "url": "/static/css/main.3248e746.chunk.css"
+  },
+  {
+    "revision": "b28e2115088ffe5af72e9dc2ffcfb94d",
+    "url": "/index.html"
+  }
+];
